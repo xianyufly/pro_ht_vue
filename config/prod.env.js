117ee725c38a11706f3513fded40fcea7757612d
xianyufly@sina.com <xianyufly@sina.com>
2018-08-23 09:08:22 +0800

    代码更新

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_ENV: '"production"',
   BASE:'"/pro_ht/"',//history 模式 访问根路径
   UE_IFRAME_URL:'"https://www.xddedu.com"',
-  API_URL:'"http://api.17sobt.com/"'//接口地址
+  API_URL:'"https://api.17sobt.com/"'//接口地址
 }
